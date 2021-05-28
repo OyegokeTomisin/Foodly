238961ad23575ea9f4785e0d5edc2ff391504b40
tomisin <tomisin18@gmail.com>
2021-05-28 13:20:47 +0100

    Add swift UI project

---
+++ Foodly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Foodly
+//
+//  Created by Oyegoke Oluwatomisin on 28/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Foodly/FoodlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodlyApp.swift
+//  Foodly
+//
+//  Created by Oyegoke Oluwatomisin on 28/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct FoodlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
